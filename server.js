e16401a56c4a6e68ea694179d21547e96d729f26
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-13 21:54:27 -0400

    setup remaining folder structure

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
-// add hbs helper functions
-const helpers = require('./utils/helpers');
-
 // require 'path' for static folder below
 const path = require('path');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// add hbs helper functions
+const helpers = require('./utils/helpers');
 
 // set up Handlebars.js as your app's template engine of choice & add helper functions:
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-// - end -
-
 // set up express-session & sequelize store:
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super cool secret',
@@ -35,12 +29,14 @@ const sess = {
 };
 
 app.use(session(sess));
-// - end -
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Static assets folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
