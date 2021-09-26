fd25964d02a9eb65bd5f9206750b0190e3222001
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-26 17:59:06 -0400

    updated README

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
 
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
